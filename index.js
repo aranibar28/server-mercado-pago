b83be760a1fd1d27f35eaa39ea833a0e0cde81aa
Gerson Aranibar <aranibargerson28@gmail.com>
2023-07-20 23:30:06 -0500

    update files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const compression = require('compression');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const axios = require('axios');
 
 app.use(cors());
 app.use(express.json());
@@ -15,17 +16,176 @@ mercadopago.configure({
   integrator_id: process.env.INTEGRADOR_ID,
 });
 
-app.get('/', (req, res) => res.json({ welcome: 'Server online' }));
+const headers = {
+  Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+};
 
-app.post('/api/checkout', async (req, res) => {
+app.get('/', (req, res) =>
+  res.json({
+    server: 'online',
+    endonpoints: [
+      {
+        post: 'http://localhost:3000/api/preapproval',
+        json: {
+          reason: 'PLAN MENSUAL',
+          payer_email: 'aranibar28@gmail.com',
+          auto_recurring: {
+            frequency: 1,
+            frequency_type: 'months',
+            billing_day: 10,
+            billing_day_proportional: false,
+            free_trial: {
+              frequency: 1,
+              frequency_type: 'months',
+            },
+            transaction_amount: 30,
+            currency_id: 'PEN',
+          },
+          back_url: 'https://www.yoursite.com',
+        },
+      },
+    ],
+  }),
+);
+
+app.get('/preapproval_plan', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.mercadopago.com/preapproval_plan/search?limit=50', { headers });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).json({ error: error.message });
+  }
+});
+
+app.get('/preapproval_plan/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await axios.get('https://api.mercadopago.com/preapproval_plan/' + id, { headers });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).json({ error: error.message });
+  }
+});
+
+app.post('/preapproval_plan', async (req, res) => {
+  const data = req.body;
+  try {
+    const response = await axios.post('https://api.mercadopago.com/preapproval_plan', data, { headers });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).json({ error: error.message });
+  }
+});
+
+app.put('/preapproval_plan/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const response = await axios.put('https://api.mercadopago.com/preapproval_plan/' + id, data, { headers });
+    res.json(response.data);
+  } catch (error) {
+    res.status(error.response ? error.response.status : 500).json({ error: error.message });
+  }
+});
+
+// CREAR PLAN
+app.post('/api/preapproval', async (req, res) => {
   let data = req.body;
   try {
-    let payload = build_data(data); // Construir preferencias
+    mercadopago.preapproval
+      .create(data)
+      .then((resp) => {
+        res.json(resp.response);
+      })
+      .catch((error) => {
+        res.status(500).json({ msg: error.message });
+      });
+  } catch (error) {
+    return res.json({ msg: error.message });
+  }
+});
 
-    mercadopago.preferences
-      .create(payload)
+// BUSCAR PLAN POR ID
+app.get('/api/preapproval_plan/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    mercadopago.preapproval
+      .findById(id)
+      .then((resp) => {
+        res.json(resp.response);
+      })
+      .catch((error) => {
+        res.status(500).json({ msg: error.message });
+      });
+  } catch (error) {
+    return res.json({ msg: error.message });
+  }
+});
+
+// BUSCAR TODOS LOS PLANES
+app.get('/api/search_plans', async (req, res) => {
+  try {
+    mercadopago.preapproval
+      .search()
       .then((response) => {
-        res.json({ id: response.body.id, payload }); // Retornar GUID
+        res.json(response);
+      })
+      .catch((error) => {
+        res.status(500).json({ msg: error.message });
+      });
+  } catch (error) {
+    return res.json({ msg: error.message });
+  }
+});
+
+// ACTUALIZAR PLAN
+app.put('/api/preapproval_plan', async (req, res) => {
+  let data = req.body;
+  try {
+    mercadopago.preapproval
+      .update(data)
+      .then((response) => {
+        res.json(response);
+      })
+      .catch((error) => {
+        res.status(500).json({ msg: error.message });
+      });
+  } catch (error) {
+    return res.json({ msg: error.message });
+  }
+});
+
+app.post('/api/card_token', async (req, res) => {
+  const cardData = {
+    card_number: '5031755734530604',
+    cardholder: {
+      name: 'Gerson Aranibar',
+      identification: {
+        type: 'DNI',
+        number: '12345699',
+      },
+    },
+    expiration_month: 11,
+    expiration_year: 2025,
+    security_code: '123',
+    email: 'aranibar28@gmail.com',
+  };
+
+  try {
+    const cardTokenResponse = await mercadopago.card_token.create(cardData);
+    res.json(cardTokenResponse.response);
+  } catch (error) {
+    return res.json({ msg: error.message });
+  }
+});
+
+app.post('/api/preapproval2', async (req, res) => {
+  let data = req.body;
+  try {
+    mercadopago.pre
+      .create(data)
+      .then((resp) => {
+        res.json(resp.response);
       })
       .catch((error) => {
         res.status(500).json({ msg: error.message });
@@ -38,21 +198,3 @@ app.post('/api/checkout', async (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log('Servidor iniciado en el puerto ' + process.env.PORT);
 });
-
-const build_data = (data) => {
-  return {
-    items: [
-      {
-        title: data.title,
-        quantity: data.quantity,
-        currency_id: data.currency_id,
-        unit_price: data.unit_price,
-      },
-    ],
-    payer: {
-      name: 'Raul Alvarez',
-      email: data.email,
-    },
-    external_reference: 'Referencia externa de la compra',
-  };
-};
